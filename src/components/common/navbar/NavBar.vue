513e8d08b5cd720f5cac106deb0009c9d3b1557c
xiaoyao <langjun>
2021-01-27 21:53:20 +0800

    及时更新

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="nav-bar">
+    	<div class="left"><slot name="left"></slot></div>
+    	<div class="center"><slot name="center"></slot></div>
+    	<div class="right"><slot name="right"></slot></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default{
+    name: "NavBar"
+  }
+</script>
+
+<style scoped>
+  .nav-bar {
+    display: flex;
+    height: 44px;
+    line-height: 44px;
+  }
+  .left, .right {
+    width: 60px;
+    /*background: rgba(0, 238, 118, .5);*/
+  }
+  .center {
+    flex: 1; 
+    /*background: rgba(132, 122, 255, .5);*/
+    text-align: center; /*此处的文字居中未写到.nav-bar里*/
+
+  }
+</style>
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="recommend">
+    <div v-for="(item,index) in recommends" :key="index" class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default{
+    name: "RecommendView",
+    props: {
+      recommends:{
+        type: Array,
+        default(){
+          return []
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .recommend {
+    display: flex;
+    width: 100%;
+    text-align: center;
+    border-bottom: 10px solid rgba(139,101,8,.2);
+  }
+  .recommend-item {
+    flex: 1;
+    font-size: 16px;
+  }
+  .recommend-item img {
+    width: 90%;
+    margin-bottom: 4px;
+  }
+  .recommend-item div {
+    margin-bottom: 3px;
+    color: rgba(255, 106, 106);
+  }
+
+</style>
